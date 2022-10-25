65a7821b0148b939e2605bc1619b4da60b01b4c8
terbeche <termus96@gmail.com>
2022-10-25 11:27:48 +0100

    Add Header for the app

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Header = () => {
+return (
+<div>
+<div>
+<h2>Doctors Apointment</h2>
+<span>Your Health means everything for us</span>
+<i></i>
+
+</div>
+<Navbar />
+
+</div>
+)
+}
+
+export default Header 
