132b5740ac95a0248b0f1f23f3cb5479a76f2c8d
terbeche <termus96@gmail.com>
2022-10-25 13:58:52 +0100

    Edit App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import NavBar from './components/Navbar';
-function App() {
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import PhysiciansList from './components/PhysiciansList'
+// import Book from './components/Book'
+// import DoctorsList from './components/DoctorsList'
+import './components/Navbar.css'
+function App () {
   return (
-
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Header />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    <div className='container'>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Header />} />
+           <Route path='/doctors' element={<PhysiciansList />} />
+          </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
 
-export default App;
+export default App
