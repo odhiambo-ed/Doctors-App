6ac2f92d91decc02a216ca4c8d3789fc3f3264b1
terbeche <termus96@gmail.com>
2022-10-25 13:59:06 +0100

    Edit Header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,16 +3,9 @@ import Navbar from './Navbar'
 
 const Header = () => {
 return (
-<div>
-<div>
-<h2>Doctors Apointment</h2>
-<span>Your Health means everything for us</span>
-<i></i>
 
-</div>
 <Navbar />
 
-</div>
 )
 }
 
