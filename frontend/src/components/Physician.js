110458dfcf5df4b2116a4c2cdd560714e811cef3
terbeche <termus96@gmail.com>
2022-10-26 10:03:08 +0100

    Creat the Single Physician page

---
+++ frontend/src/components/Physician.js
@@ -0,0 +1,40 @@
+/* eslint-disable camelcase */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Physician = ({
+  id, name, bio, photo, specialization, city, consultation_fee,
+}) => (
+  <div key={id} className="doctor-container">
+    <div>
+      <img src={photo} alt="Doctor" className="doctor-image" />
+
+    </div>
+    <div className="doctor-info">
+      <div className="doctor-name">{name}</div>
+      <div className="doctor-specialization">{specialization}</div>
+      <div className="doctor-fee">
+        <span>Consultation Price:  </span>
+        {consultation_fee}
+        $
+      </div>
+      <div className="doctor-bio">{bio}</div>
+      <div className="doctor-city">{city}</div>
+    <button type="button">Book Appointment</button>
+    </div>
+  </div>
+
+);
+
+Physician.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  bio: PropTypes.string.isRequired,
+  specialization: PropTypes.string.isRequired,
+  photo: PropTypes.node.isRequired,
+  city: PropTypes.string.isRequired,
+  consultation_fee: PropTypes.number.isRequired,
+
+};
+
+export default Physician;
