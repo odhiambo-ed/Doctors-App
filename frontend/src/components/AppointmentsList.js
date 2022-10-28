91e721b52bcdfc0e5a230b873a47e010c9df3a7e
Pazzo97 <pazzomuk@gmail.com>
2022-10-28 11:39:19 +0200

    Add appointment list page

---
+++ frontend/src/components/AppointmentsList.js
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchAppointments } from '../redux/appointments/appointmentSlice';
+import AppointmentDetail from './AppointmentDetail';
+import './Appointment.css';
+
+const AppointmentsList = () => {
+  const dispatch = useDispatch();
+  const { loading } = useSelector((state) => state.appointments);
+  useEffect(() => {
+    async function fetchAllAppointments() {
+      dispatch(fetchAppointments());
+    }
+    fetchAllAppointments();
+  }, []);
+  return (
+    <section>
+      {loading && <h2>Loading...</h2>}
+      {!loading && <AppointmentDetail />}
+    </section>
+  );
+};
+export default AppointmentsList;
