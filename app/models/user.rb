e2c16ec94cdadaf8daea7ad7fb2490e4951b8ce6
Pazzo97 <pazzomuk@gmail.com>
2022-10-28 11:36:52 +0200

    Modify User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_many :appointments
   has_many :physicians, through: :appointments
-
   validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 25 }
-  validates :email, presence: true, length: { maximum: 60 }
+  # validates :email, presence: true, length: { maximum: 60 }
 end
