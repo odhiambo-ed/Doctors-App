31504f163a771074aeb096c7b9235e5d69eb42af
terbeche <termus96@gmail.com>
2022-10-29 13:02:38 +0100

    Edit physician controller

--- app/controllers/physicians_controller.rb
+++ app/controllers/physicians_controller.rb
@@ -18,11 +18,12 @@ class PhysiciansController < ApplicationController
     @physician = Physician.new(physician_params)
 
     if @physician.save
-      render json: @physician, status: :created, location: @physician
+      render json: @physician, status: :created
     else
       render json: @physician.errors, status: :unprocessable_entity
     end
   end
+  
 
   # PATCH/PUT /physicians/1
   def update
@@ -37,7 +38,7 @@ class PhysiciansController < ApplicationController
   def destroy
     @physician.destroy
   end
-
+  
   private
 
   # Use callbacks to share common setup or constraints between actions.
