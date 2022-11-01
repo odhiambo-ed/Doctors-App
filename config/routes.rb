8894148e4f7612b33a25e0fcc4f2ef3c11efc4d3
white3d <odhiambo.edward111@gmail.com>
2022-11-01 16:40:27 +0300

    Edit Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  resources :appointments
-  resources :physicians
-  resources :users
+  # resources :appointments
+  # resources :physicians
+  # resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      post '/users', to: 'users#create'
+    post '/login', to: 'users#login'
+    resources :physicians
+    resources :appointments
+    end
+  end
 
   # Defines the root path route ("/")
   # root "articles#index"
