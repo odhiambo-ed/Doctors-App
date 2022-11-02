abb2b2f310df69fcbbacc2dec70a21d08af389bf
terbeche <termus96@gmail.com>
2022-11-02 06:00:17 +0100

    Create slices for authentication

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,12 +2,17 @@ import { configureStore } from '@reduxjs/toolkit';
 import physicians from './physicians/physicianSlice';
 import users from './users/userSlice';
 import appointments from './appointments/appointmentSlice';
+import authReducer from '../slices/auth';
+import messageReducer from '../slices/message';
 
 const store = configureStore({
   reducer: {
     physicians,
     users,
     appointments,
+    auth: authReducer,
+    message: messageReducer,
+    devTools: true,
   },
 });
 
