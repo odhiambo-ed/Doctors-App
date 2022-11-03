ad665aea4616704661702a6aa4ecdf802b81c844
white3d <odhiambo.edward111@gmail.com>
2022-11-03 18:02:35 +0300

    Add Style To Login Page

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -34,7 +34,12 @@ const Login = () => {
   };
   return (
     <div className="subWindow">
-      <div className="navigationWindow">
+      <div
+        className="navigationWindow"
+        style={{
+          width: show ? '18%' : '0%',
+        }}
+      >
         {show ? (
           <div>
             <div className="navTop">
