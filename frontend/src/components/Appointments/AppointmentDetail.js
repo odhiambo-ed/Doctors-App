9d51b0c0f6e4aad100c2a8a7d3161e1396b902dd
white3d <odhiambo.edward111@gmail.com>
2022-11-03 17:59:04 +0300

    Add Appointment Details

--- frontend/src/components/Appointments/AppointmentDetail.js
+++ frontend/src/components/Appointments/AppointmentDetail.js
@@ -1,23 +1,41 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import { useSelector } from 'react-redux';
 import AppointmentPage from './AppointmentPage';
-import './Appointment.css';
 
 const AppointmentDetail = () => {
   const { appointmentList } = useSelector((state) => state.appointments);
-  console.log(appointmentList);
   return (
-    <div className="appointment-list">
-      {appointmentList.map((appointment) => (
-        <AppointmentPage
-          key={appointment.id}
-          id={appointment.id}
-          reason={appointment.reason}
-          date={appointment.date}
-          time={appointment.time}
-          doctor={appointment.physician_id}
-        />
-      ))}
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        {appointmentList.map((appointment) => (
+          <Grid
+            item
+            xs={2}
+            sm={4}
+            md={4}
+            key={appointment.id}
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <AppointmentPage
+              id={appointment.id}
+              reason={appointment.reason}
+              date={appointment.date}
+              time={appointment.time}
+              photo={appointment.physician_id}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 };
 export default AppointmentDetail;
