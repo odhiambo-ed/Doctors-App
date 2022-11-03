dad2dd5cf9317fdfd2df9c1393d763704d86b7ba
white3d <odhiambo.edward111@gmail.com>
2022-11-03 16:13:30 +0300

    Updated App JS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,39 +7,30 @@ import AddPhysicianForm from './components/Physicians/AddPhysicianForm';
 import DeletePhysician from './components/Physicians/DeletePhysician';
 import BookAppointmentForm from './components/Appointments/BookAppointmentForm';
 import AppointmentsList from './components/Appointments/AppointmentsList';
+import Login from './components/login/Login';
+import Register from './components/register/Register';
 import './components/Navbar.css';
-import Login from './components/Login';
-import Register from './components/Register';
-import Home from './components/Home';
-import Profile from './components/Profile';
-import BoardUser from './components/BoardUser';
-import BoardModerator from './components/BoardModerator';
-import BoardAdmin from './components/BoardAdmin';
 
-const App = () => (
-  <div className="container">
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Header />}>
-          <Route index element={<PhysiciansList />} />
-          <Route path="/doctors" element={<PhysiciansList />} />
-          <Route path="/appointments" element={<AppointmentsList />} />
-          <Route path="/doctors/:id" element={<Physician />} />
-          <Route path="/doctors/new" element={<AddPhysicianForm />} />
-          <Route path="/doctors/delete" element={<DeletePhysician />} />
-          <Route path="/doctors/book" element={<BookAppointmentForm />} />
-          <Route path="/doctors/:id/book" element={<BookAppointmentForm />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/user" element={<BoardUser />} />
-          <Route path="/mod" element={<BoardModerator />} />
-          <Route path="/admin" element={<BoardAdmin />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </div>
-);
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Header />}>
+            <Route index element={<PhysiciansList />} />
+            <Route path="/doctors" element={<PhysiciansList />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/appointments" element={<AppointmentsList />} />
+            <Route path="/doctors/:id" element={<Physician />} />
+            <Route path="/doctors/new" element={<AddPhysicianForm />} />
+            <Route path="/doctors/delete" element={<DeletePhysician />} />
+            <Route path="/doctors/book" element={<BookAppointmentForm />} />
+            <Route path="/doctors/:id/book" element={<BookAppointmentForm />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
 export default App;
